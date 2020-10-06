5813c77978723970f8c7bff2dbb224703b780761
alejandropajaronjorquera <a.pajaron.jorquera@dkbcodefactory.com>
2020-10-06 09:45:30 +0200

    add dish factory.

---
+++ src/main/java/chapter06/DishFactory.java
@@ -0,0 +1,36 @@
+package chapter06;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
+
+public class DishFactory {
+
+    public static final List<Dish> menu = Arrays.asList(
+            new Dish("pork", false, 800, Type.MEAT),
+            new Dish("beef", false, 700, Type.MEAT),
+            new Dish("chicken", false, 400, Type.MEAT),
+            new Dish("french fries", true, 530, Type.OTHER),
+            new Dish("rice", true, 350, Type.OTHER),
+            new Dish("season fruit", true, 120, Type.OTHER),
+            new Dish("pizza", true, 550, Type.OTHER),
+            new Dish("prawns", false, 400, Type.FISH),
+            new Dish("salmon", false, 450, Type.FISH));
+
+    public static final Map<String, List<String>> dishTags = new HashMap<>();
+    static {
+        dishTags.put("pork", asList("greasy", "salty"));
+        dishTags.put("beef", asList("salty", "roasted"));
+        dishTags.put("chicken", asList("fried", "crisp"));
+        dishTags.put("french fries", asList("greasy", "fried"));
+        dishTags.put("rice", asList("light", "natural"));
+        dishTags.put("season fruit", asList("fresh", "natural"));
+        dishTags.put("pizza", asList("tasty", "salty"));
+        dishTags.put("prawns", asList("tasty", "roasted"));
+        dishTags.put("salmon", asList("delicious", "fresh"));
+    }
+
+}
