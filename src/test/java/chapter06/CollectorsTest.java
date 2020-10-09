4e3e100d96e998fe6a975fe59dfbbeb49dbcdfa7
alejandropajaronjorquera <a.pajaron.jorquera@dkbcodefactory.com>
2020-10-09 22:33:33 +0200

    more examples on collectors

--- src/test/java/chapter06/CollectorsTest.java
+++ src/test/java/chapter06/CollectorsTest.java
@@ -3,12 +3,12 @@ package chapter06;
 import org.junit.jupiter.api.Test;
 
 import java.util.Comparator;
+import java.util.IntSummaryStatistics;
 import java.util.function.BinaryOperator;
 
 import static chapter06.DishFactory.menu;
 import static chapter06.DishHelper.dishCaloriesComprarator;
-import static java.util.stream.Collectors.counting;
-import static java.util.stream.Collectors.reducing;
+import static java.util.stream.Collectors.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CollectorsTest {
@@ -34,4 +34,40 @@ public class CollectorsTest {
         Dish result = menu.stream().collect(reducing(moreCaloricOf)).get();
         assertThat(result).isEqualToComparingFieldByField(expected);
     }
+
+    @Test
+    public void calculateTotalCalories(){
+        int totalCalories = menu.stream().collect(summingInt(Dish::getCalories));
+        assertThat(totalCalories).isEqualTo(4300);
+    }
+
+    @Test
+    public void testCalculateAverageCalories(){
+        Double averageCalories = menu.stream().collect(averagingInt(Dish::getCalories));
+        assertThat(averageCalories).isEqualTo(477.77777777777777);
+    }
+
+    @Test
+    public void testShowStatistics(){
+        IntSummaryStatistics intSummaryStatistics = menu.stream().collect(summarizingInt(Dish::getCalories));
+        System.out.println(intSummaryStatistics.toString());
+        assertThat(intSummaryStatistics.getAverage()).isEqualTo(477.77777777777777);
+        assertThat(intSummaryStatistics.getCount()).isEqualTo(9);
+        assertThat(intSummaryStatistics.getMax()).isEqualTo(800);
+        assertThat(intSummaryStatistics.getMin()).isEqualTo(120);
+        assertThat(intSummaryStatistics.getSum()).isEqualTo(4300);
+    }
+
+    @Test
+    public void test(){
+        String menuDescription = menu.stream().map(Dish::getName).collect(joining());
+        assertThat(menuDescription).isEqualTo("porkbeefchickenfrench friesriceseason fruitpizzaprawnssalmon");
+    }
+
+    @Test
+    public void test2(){
+        String menuDescription = menu.stream().map(Dish::getName).collect(joining(","));
+        assertThat(menuDescription).isEqualTo("pork,beef,chicken,french fries,rice,season fruit,pizza,prawns,salmon");
+    }
+
 }
