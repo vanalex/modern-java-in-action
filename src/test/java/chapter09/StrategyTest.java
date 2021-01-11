33fec8b1c7b74f33f4a4e50262fde602f6d83cba
alejandropajaronjorquera <a.pajaron.jorquera@dkbcodefactory.com>
2021-01-11 20:59:26 +0100

    test strategy pattern with lambdas.

--- src/test/java/chapter09/StrategyTest.java
+++ src/test/java/chapter09/StrategyTest.java
@@ -3,6 +3,7 @@ package chapter09;
 import chapter09.strategy.IsAllowerCase;
 import chapter09.strategy.IsNumeric;
 import chapter09.strategy.ValidationStrategy;
+import chapter09.strategy.Validator;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,5 +18,11 @@ public class StrategyTest {
         strategy = new IsAllowerCase();
         assertThat(strategy.execute("aasd")).isTrue();
         assertThat(strategy.execute("aasD")).isFalse();
+
+        Validator validator = new Validator((String s) -> s.matches("\\d+"));
+        assertThat(validator.validate("aaaa")).isFalse();
+        validator = new Validator((String s) -> s.matches("[a-z]+"));
+        assertThat(validator.validate("bbbb")).isTrue();
+
     }
 }
