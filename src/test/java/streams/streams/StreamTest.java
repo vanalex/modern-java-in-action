7d732cf577f2f8484f73d2606c3b16b00b750cd8
alejandropajaronjorquera <a.pajaron.jorquera@dkbcodefactory.com>
2021-03-27 09:23:51 +0100

    add test on concat 3 streams with empty one

--- src/test/java/streams/streams/StreamTest.java
+++ src/test/java/streams/streams/StreamTest.java
@@ -35,4 +35,17 @@ class StreamTest {
         List<String> allThree = Stream.concat(Stream.concat(first, second), third).collect(Collectors.toList());
         assertThat(allThree).isEqualTo(List.of("a", "b", "c", "1", "2", "3", "alpha", "beta", "gamma"));
     }
+
+    @Test
+    void testConcatenateEmptyStream(){
+        List<String> expected = List.of("a","b","c","X","Y", "Z","alpha", "beta", "gamma");
+        Stream<String> first = Stream.of("a", "b", "c");
+        Stream<String> second = Stream.of("X", "Y", "Z");
+        Stream<String> third = Stream.of("alpha", "beta", "gamma");
+        Stream<String> fourth = Stream.empty();
+
+        List<String> total = Stream.of(first, second, third, fourth)
+                .reduce(Stream.empty(), Stream::concat).collect(Collectors.toList());
+        assertThat(total).isEqualTo(expected);
+    }
 }
