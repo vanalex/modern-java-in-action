f095e97d8e92527330064c96294cbd2a5a7f9927
alejandropajaronjorquera <a.pajaron.jorquera@dkbcodefactory.com>
2021-03-31 07:11:50 +0200

    add tests on collections count

---
+++ src/test/java/streams/streams/CoutingElementsTest.java
@@ -0,0 +1,25 @@
+package streams.streams;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CoutingElementsTest {
+
+    @Test
+    void testStreamCount(){
+        long count = Stream.of(3, 1, 4, 1, 5, 9, 2, 6, 5)
+                .count();
+        assertThat(count).isEqualTo(9);
+    }
+
+    @Test
+    void testStreamCollectorCount(){
+        long count = Stream.of(3, 1, 4, 1, 5, 9, 2, 6, 5)
+                .collect(Collectors.counting());
+        assertThat(count).isEqualTo(9);
+    }
+}
