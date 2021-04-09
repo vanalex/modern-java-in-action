6515e7a4685717b9cea6ed0b70e8d9e319564365
vanalex <apjorquera@gmail.com>
2021-04-09 22:22:05 +0200

    read json and apply to dataframe

--- src/test/java/spark/SparkTest.java
+++ src/test/java/spark/SparkTest.java
@@ -1,6 +1,8 @@
 package spark;
 
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -9,11 +11,11 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static spark.SparkContextBuilder.SPARK_CONTEXT;
+import static spark.SparkSessionBuilder.SPARK_SESSION;
 
 public class SparkTest {
 
     @Test
-    @Disabled
     public void testSum(){
         List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
         JavaRDD<Integer> distData = SPARK_CONTEXT.parallelize(data);
@@ -22,11 +24,16 @@ public class SparkTest {
     }
 
     @Test
-    @Disabled
     public void testGetTextLength(){
         JavaRDD<String> lines =  SPARK_CONTEXT.textFile("src/main/resources/spark/data.txt");
         JavaRDD<Integer> lineLengths = lines.map(s -> s.length());
         int totalLength = lineLengths.reduce((a, b) -> a + b);
         assertThat(totalLength).isEqualTo(20);
+    }
+
+    @Test
+    public void testGetJson(){
+        Dataset<Row> df =  SPARK_SESSION.read().json("src/main/resources/spark/people.json");
+        df.show();
     }
 }
