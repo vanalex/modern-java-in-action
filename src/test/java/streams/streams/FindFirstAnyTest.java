ab46f8ce4b3cd5487c9ef3f8fad93c0ab9c3584e
alejandropajaronjorquera <a.pajaron.jorquera@dkbcodefactory.com>
2021-04-14 07:35:30 +0200

    add test on streams sorted and sequential

--- src/test/java/streams/streams/FindFirstAnyTest.java
+++ src/test/java/streams/streams/FindFirstAnyTest.java
@@ -2,7 +2,9 @@ package streams.streams;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,4 +32,46 @@ class FindFirstAnyTest {
         assertThat(firstEvenGT10).isEqualTo(Optional.empty());
     }
 
+    @Test
+    void testUnordered() {
+        List<Integer> expected = List.of(3, 1, 4, 1, 5, 9, 2, 6, 5);
+        List<Integer> result = Stream.of(3, 1, 4, 1, 5, 9, 2, 6, 5)
+                .unordered()
+                .collect(Collectors.toList());
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void testOrdered() {
+        List<Integer> expected = List.of(1, 1, 2, 3, 4, 5, 5, 6, 9);
+        List<Integer> result = Stream.of(3, 1, 4, 1, 5, 9, 2, 6, 5)
+                .sorted()
+                .collect(Collectors.toList());
+        assertThat(result).isEqualTo(expected);
+
+        Optional<Integer> any = Stream.of(3, 1, 4, 1, 5, 9, 2, 6, 5)
+                .unordered()
+                .map(FindFirstAnyTest::delay)
+                .findAny();
+
+        System.out.println("Sequential Any: " + any);
+    }
+
+    @Test
+    void testSequentialAny() {
+        Optional<Integer> any = Stream.of(3, 1, 4, 1, 5, 9, 2, 6, 5)
+                .unordered()
+                .map(FindFirstAnyTest::delay)
+                .findAny();
+        assertThat(any).isEqualTo(Optional.of(3));
+    }
+
+    private static Integer delay(Integer n) {
+        try {
+            System.out.println(Thread.currentThread().getName());
+            Thread.sleep((long) (Math.random() * 100));
+        } catch (InterruptedException ignored) {
+        }
+        return n;
+    }
 }
