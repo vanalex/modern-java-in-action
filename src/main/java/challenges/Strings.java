e77ca87ca8d65d1e3d9eb322e2d1d3c9efc4cc29
vanalex <apjorquera@gmail.com>
2021-04-23 06:29:58 +0200

    first non repeatable char

--- src/main/java/challenges/Strings.java
+++ src/main/java/challenges/Strings.java
@@ -1,7 +1,9 @@
 package challenges;
 
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Strings {
@@ -20,5 +22,26 @@ public class Strings {
         return str.codePoints()
                 .mapToObj(c -> String.valueOf(Character.toChars(c)))
                 .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+    }
+
+    public static String firstNonRepeatedCharacter(String str) {
+
+        if (str == null || str.isBlank()) {
+            // or throw IllegalArgumentException
+            return String.valueOf(Character.MIN_VALUE);
+        }
+
+        Map<Integer, Long> chs = str.codePoints()
+                .mapToObj(cp -> cp)
+                .collect(Collectors.groupingBy(Function.identity(),
+                        LinkedHashMap::new, Collectors.counting()));
+
+        int cp = chs.entrySet().stream()
+                .filter(e -> e.getValue() == 1L)
+                .findFirst()
+                .map(Map.Entry::getKey)
+                .orElse(Integer.valueOf(Character.MIN_VALUE));
+
+        return String.valueOf(Character.toChars(cp));
     }
 }
--- src/test/java/challenges/StringsTest.java
+++ src/test/java/challenges/StringsTest.java
@@ -23,4 +23,17 @@ class StringsTest {
         Map<String, Long> result = Strings.countDuplicateCharacters(TEXT);
         assertThat(result.get("a")).isEqualTo(5);
     }
+
+    @Test
+    void testFirstNonRepeatableChar(){
+
+        String text = "My high school, the Illinois Mathematics and Science Academy, "
+                + "showed me that anything is possible and that you're never too young to think big. "
+                + "At 15, I worked as a computer programmer at the Fermi National Accelerator Laboratory, "
+                + "or Fermilab. After graduating, I attended Stanford for a degree in economics and "
+                + "computer science.";
+
+        String result = Strings.firstNonRepeatedCharacter(text);
+        assertThat(result).isEqualTo("'");
+    }
 }
