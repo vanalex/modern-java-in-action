3cb3b540a3071fee9db0be8d48aadda35b5a5a4a
alex <alex.pajaron@vier.ai>
2021-11-13 22:47:42 +0100

    adding tests

--- src/test/java/chapter02/FilterTest.java
+++ src/test/java/chapter02/FilterTest.java
@@ -1,5 +1,6 @@
 package chapter02;
 
+import static chapter02.FilterFactory.filter;
 import static chapter02.FilterFactory.filterApplesByColor;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static utils.TestFixture.createList;
@@ -16,10 +17,38 @@ class FilterTest {
 
   @ParameterizedTest
   @ArgumentsSource(FilterTest.AppleProvider.class)
-    void test(List<Apple> inventory){
+    void testFilterApplesByColorGreen(List<Apple> inventory){
       List<Apple> result = filterApplesByColor(inventory, Color.GREEN);
       assertThat(result).hasSameElementsAs( createList(new Apple(80, Color.GREEN), new Apple(155, Color.GREEN)));
     }
+
+  @ParameterizedTest
+  @ArgumentsSource(FilterTest.AppleProvider.class)
+  void testFilterApplesByColorRed(List<Apple> inventory){
+    List<Apple> result = filterApplesByColor(inventory, Color.RED);
+    assertThat(result).hasSameElementsAs( createList(new Apple(120, Color.RED)));
+  }
+
+  @ParameterizedTest
+  @ArgumentsSource(FilterTest.AppleProvider.class)
+  void testFilterApples(List<Apple> inventory){
+    List<Apple> result = filter(inventory, new AppleColorPredicate());
+    assertThat(result).hasSameElementsAs( createList(new Apple(80, Color.GREEN), new Apple(155, Color.GREEN)));
+  }
+
+  @ParameterizedTest
+  @ArgumentsSource(FilterTest.AppleProvider.class)
+  void testFilterApplesByWeight(List<Apple> inventory){
+    List<Apple> result = filter(inventory, new AppleWeightPredicate());
+    assertThat(result).hasSameElementsAs( createList(new Apple(155, Color.GREEN)));
+  }
+
+  @ParameterizedTest
+  @ArgumentsSource(FilterTest.AppleProvider.class)
+  void testFilterApplesByColorAndWeight(List<Apple> inventory){
+    List<Apple> result = filter(inventory, new AppleRedAndHeavyPredicate());
+    assertThat(result).hasSameElementsAs( createList());
+  }
 
   static class AppleProvider implements ArgumentsProvider {
 
