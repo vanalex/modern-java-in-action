c2c8768825ce85ba08d5c898325dd353a0c61f3d
alex <alex.pajaron@vier.ai>
2022-03-19 22:34:32 +0100

    add example with predicate

--- src/test/java/collections/MutableTest.java
+++ src/test/java/collections/MutableTest.java
@@ -3,6 +3,8 @@ package collections;
 import org.assertj.core.api.Assertions;
 import org.eclipse.collections.api.factory.Lists;
 import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.impl.block.factory.Predicates;
+import org.eclipse.collections.impl.list.mutable.FastList;
 import org.junit.jupiter.api.Test;
 
 public class MutableTest {
@@ -18,5 +20,12 @@ public class MutableTest {
 
     MutableList<String> mutableList2 = Lists.mutable.of("One", "Two", "Three");
     Assertions.assertThat(mutableList2.size()).isEqualTo(3);
+  }
+
+  @Test
+  void testFastList(){
+    MutableList<Integer> list = FastList.newListWith(1, 8, 5, 41, 31, 17, 23, 38);
+    boolean result = list.allSatisfy(Predicates.greaterThan(0));
+    Assertions.assertThat(result).isTrue();
   }
 }
