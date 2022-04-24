9196ec632975017f25c1ab2cf8549b2c84f694b9
alex <alex.pajaron@vier.ai>
2022-04-24 22:41:37 +0200

    change creation of list

--- src/test/java/challenges/immutability/NumbersTest.java
+++ src/test/java/challenges/immutability/NumbersTest.java
@@ -1,6 +1,6 @@
 package challenges.immutability;
 
-import challenges.immutability.Numbers;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,14 +9,14 @@ class NumbersTest {
 
   @Test
   void testSumIntegers(){
-    List<Integer> integers = List.of(3, 2, 6 , null, 9);
+    List<Integer> integers = Arrays.asList(3, 2, 6, null, 9);
     int result = Numbers.sumIntegers(integers);
     Assertions.assertEquals(20, result);
   }
 
   @Test
   void testIntegersContainNull(){
-    List<Integer> integers = List.of(3, 2, 6 , null, 9);
+    List<Integer> integers = Arrays.asList(3, 2, 6 , null, 9);
     boolean result = Numbers.integersContainsNulls(integers);
     Assertions.assertTrue(result);
   }
