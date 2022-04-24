f8bca77ceb05c13ea980a5601c417ccb518810a5
alex <alex.pajaron@vier.ai>
2022-04-24 21:48:08 +0200

    refactor classes and add numbers challenges

--- src/main/java/challenges/numbers/Numbers.java
+++ src/main/java/challenges/numbers/Numbers.java
@@ -1,48 +1,22 @@
 package challenges.numbers;
 
-import java.util.function.BinaryOperator;
+import java.util.List;
+import java.util.Objects;
 
-public class Numbers {
+public final class Numbers {
 
-    private static BinaryOperator<Integer> operator = Math::addExact;
-
-    public static int sumClassicWay(int num1, int num2){
-        return num1 + num2;
+    private Numbers(){
+        throw new AssertionError("Cannot be instantiated");
     }
 
-    public static int sumWithInteger(int num1, int num2){
-        return Integer.sum(num1, num2);
-    }
+    public static int sumIntegers(List<Integer> integers) {
 
-    public static int sumWithMath(int num1, int num2){
-        return Math.addExact(num1, num2);
-    }
+        if (Objects.isNull(integers)) {
+            throw new IllegalArgumentException("List cannot be null");
+        }
 
-    public static int sumWithOperator(int num1, int num2){
-        return operator.apply(num1, num2);
-    }
-
-    public static int minWithInteger(int number1, int number2){
-        return new IntegerOperation<>(number1, number2).min();
-    }
-
-    public static int minWithMath(int number1, int number2){
-        return new MathOperation<>(number1, number2).min();
-    }
-
-    public static int maxWithInteger(int number1, int number2){
-        return new IntegerOperation<>(number1, number2).max();
-    }
-
-    public static int maxWithMath(int number1, int number2){
-        return new MathOperation<>(number1, number2).max();
-    }
-
-    public static int maxWithBinaryOperator(int number1, int number2){
-        return new BinaryOperation<>(number1, number2).max();
-    }
-
-    public static int minWithBinaryOperator(int number1, int number2){
-        return new BinaryOperation<>(number1, number2).min();
+        return integers.stream()
+            .filter(Objects::nonNull)
+            .mapToInt(Integer::intValue).sum();
     }
 }
--- src/test/java/challenges/numbers/NumbersTest.java
+++ src/test/java/challenges/numbers/NumbersTest.java
@@ -1,63 +1,15 @@
 package challenges.numbers;
 
-import challenges.numbers.Numbers;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class NumbersTest {
 
-    @Test
-    void testSumClassicWay(){
-        int result = Numbers.sumClassicWay(5, 3);
-        assertThat(result).isEqualTo(8);
-    }
-
-    @Test
-    void testSumWithInteger(){
-        int result = Numbers.sumWithInteger(5, 3);
-        assertThat(result).isEqualTo(8);
-    }
-
-    @Test
-    void testSumWithMath(){
-        int result = Numbers.sumWithMath(5, 3);
-        assertThat(result).isEqualTo(8);
-    }
-
-    @Test
-    void testSumWithOperator(){
-        int result = Numbers.sumWithOperator(5, 3);
-        assertThat(result).isEqualTo(8);
-    }
-
-    @Test
-    void testMinWithInteger(){
-        assertThat(Numbers.minWithInteger(5,3)).isEqualTo(3);
-    }
-
-    @Test
-    void testMinWithMath(){
-        assertThat(Numbers.minWithMath(5,3)).isEqualTo(3);
-    }
-
-    @Test
-    void testMaxWithInteger(){
-        assertThat(Numbers.maxWithInteger(5,3)).isEqualTo(5);
-    }
-
-    @Test
-    void testMaxWithMath(){
-        assertThat(Numbers.maxWithMath(5,3)).isEqualTo(5);
-    }
-
-    @Test
-    void testMaxWithBinaryOperator(){
-        assertThat(Numbers.maxWithBinaryOperator(5,3)).isEqualTo(5);
-    }
-
-    @Test
-    void testMinWithBinaryOperator(){
-        assertThat(Numbers.minWithBinaryOperator(5,3)).isEqualTo(3);
-    }
+  @Test
+  void testSumIntegers(){
+    List<Integer> integers = List.of(3, 2, 6 , null, 9);
+    int result = Numbers.sumIntegers(integers);
+    Assertions.assertEquals(20, result);
+  }
 }
